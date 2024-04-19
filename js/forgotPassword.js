de5151fc29e2a2f0173d75e81084651ebfd78287
Pham Van hinh <20102004hinh@gmail.com>
2024-04-19 16:58:25 +0700

    chinh sua file forgot password  và chỉnh sửa conflic

--- js/forgotPassword.js
+++ js/forgotPassword.js
@@ -2,14 +2,16 @@ $(document).ready(function(){
     if($(".button_js").click(function(event){
         event.preventDefault();
         var pattern = /^[0-9A-Za-z]+@[a-z]{4,}\.com$/;
-        var value = $("#email").val();
+        var value = $("#enterEmail").val();
         if(pattern.test(value)){
             alert("chúng tôi sẽ gửi mail cho bạn trong 2-3 phút nữa");
             // Chuyển hướng sang trang cục bộ trên máy tính
             window.location.href = "../html/signIn.html";
+            return true;
         }
         else{
             alert("bạn phải nhấn như mẫu");
+            return false;
         }
     }));
 });
